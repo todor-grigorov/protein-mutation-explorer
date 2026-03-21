42fba098e14cabf460c9a9768ddc30d1c8cde8f5
todor-grigorov <t.grigorov99@gmail.com>
2026-03-21 22:34:16 +0100

    feat: configure Service Extensions and register them in Program.cs (Api)

---
+++ backend/ProteinMutation/ProteinMutation.Api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProteinMutation.Domain.Repositories;
+using ProteinMutation.Infrastructure.Options;
+using ProteinMutation.Infrastructure.Persistence;
+using ProteinMutation.Infrastructure.Persistence.Repositories;
+using ProteinMutation.Infrastructure.Persistence.Seed;
+
+namespace ProteinMutation.Api.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .WithExposedHeaders("X-Pagination"));
+            });
+        }
+
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISServerOptions>(options =>
+            {
+                //options.AllowSynchronousIO = true;
+            });
+        }
+
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+            services.AddDbContext<AppDbContext>(options =>
+                    options.UseSqlite(configuration.GetSection(DatabaseOptions.SectionName).Get<DatabaseOptions>()!.ConnectionString));
+
+        public static void ConfigureRepository(this IServiceCollection services) => services.AddScoped<IProteinVariantRepository, ProteinVariantRepository>();
+
+        public static void ConfigureDatabaseSeeding(this IServiceCollection services)
+        {
+            services.AddScoped<TsvVariantImporter>();
+            services.AddScoped<DatabaseInitializer>();
+        }
+    }
+}
--- backend/ProteinMutation/ProteinMutation.Api/Program.cs
+++ backend/ProteinMutation/ProteinMutation.Api/Program.cs
@@ -1,12 +1,27 @@
+using ProteinMutation.Api.Extensions;
+using ProteinMutation.Infrastructure.Persistence.Seed;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.ConfigureRepository();
+builder.Services.ConfigureDatabaseSeeding();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+// Run migrations + seeding on startup
+using (var scope = app.Services.CreateScope())
+{
+    var initializer = scope.ServiceProvider.GetRequiredService<DatabaseInitializer>();
+    await initializer.InitializeAsync();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
