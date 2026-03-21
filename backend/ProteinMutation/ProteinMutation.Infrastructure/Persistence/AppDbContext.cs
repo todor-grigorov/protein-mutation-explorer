fb3f0d66096709e4a1fc9128479299141fd9c012
todor-grigorov <t.grigorov99@gmail.com>
2026-03-21 21:52:31 +0100

    feat: add AppDbContext class (Infrastructure)

---
+++ backend/ProteinMutation/ProteinMutation.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProteinMutation.Domain.Entities;
+
+namespace ProteinMutation.Infrastructure.Persistence
+{
+    public sealed class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<ProteinVariant> ProteinVariants => Set<ProteinVariant>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
