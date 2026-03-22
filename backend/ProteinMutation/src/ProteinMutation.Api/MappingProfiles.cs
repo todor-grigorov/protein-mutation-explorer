426b71d3168f2e5b1c3aeea5f2d5bda74ea15397
todor-grigorov <t.grigorov99@gmail.com>
2026-03-22 18:05:43 +0100

    reafctor: update Mapping Profiles

--- backend/ProteinMutation/src/ProteinMutation.Api/MappingProfiles.cs
+++ backend/ProteinMutation/src/ProteinMutation.Api/MappingProfiles.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using ProteinMutation.Api.Contracts.Responses;
 using ProteinMutation.Application.DataTransferObjects;
 using ProteinMutation.Domain.Entities;
 
@@ -8,6 +9,7 @@ namespace ProteinMutation.Api
     {
         public MappingProfiles()
         {
+            // Domain → Application DTO
             CreateMap<ProteinVariant, ProteinVariantDto>()
                 .ForMember(dest => dest.VariantId,
                     opt => opt.MapFrom(src => src.VariantId.RawValue))
@@ -20,8 +22,33 @@ namespace ProteinMutation.Api
                 .ForMember(dest => dest.ToAminoAcid,
                     opt => opt.MapFrom(src => src.VariantId.ToAminoAcid));
 
+            // Application DTO → API Response
+            CreateMap<ProteinVariantDto, ProteinVariantResponse>()
+                .ForMember(dest => dest.AmClass,
+                    opt => opt.MapFrom(src => src.AmClass.ToString()))
+                .ForMember(dest => dest.Esm1bIsPathogenic,
+                    opt => opt.MapFrom(src => src.Esm1bIsPathogenic.ToString()))
+                .ForMember(dest => dest.MechanisticLabel,
+                    opt => opt.MapFrom(src => src.MechanisticLabel.ToString()));
+
+            // string → ProteinDto
             CreateMap<string, ProteinDto>()
                 .ConstructUsing(src => new ProteinDto(src));
+
+            // ProteinDto → ProteinResponse
+            CreateMap<ProteinDto, ProteinResponse>();
+
+            // InvalidVariantEntry → InvalidVariantResponse
+            CreateMap<InvalidVariantEntry, InvalidVariantResponse>();
+
+            // BatchSubmissionResult → BatchSubmissionResponse
+            CreateMap<BatchSubmissionResult, BatchSubmissionResponse>()
+                .ForMember(dest => dest.Found,
+                    opt => opt.MapFrom(src => src.Found))
+                .ForMember(dest => dest.NotFound,
+                    opt => opt.MapFrom(src => src.NotFound))
+                .ForMember(dest => dest.Invalid,
+                    opt => opt.MapFrom(src => src.Invalid));
         }
     }
 }
