f6e36d744d959d67b0cdf93d12be7a6a43a230d3
todor-grigorov <t.grigorov99@gmail.com>
2026-03-22 12:57:52 +0100

    feat: add DTOs to Application layer

---
+++ backend/ProteinMutation/src/ProteinMutation.Application/DataTransferObjects/ProteinDto.cs
@@ -0,0 +1,4 @@
+﻿namespace ProteinMutation.Application.DataTransferObjects
+{
+    public sealed record ProteinDto(string ProteinId);
+}
---
+++ backend/ProteinMutation/src/ProteinMutation.Application/DataTransferObjects/ProteinVariantDto.cs
@@ -0,0 +1,33 @@
+﻿namespace ProteinMutation.Application.DataTransferObjects
+{
+    public sealed record ProteinVariantDto(
+    string VariantId,
+    string ProteinId,
+    char FromAminoAcid,
+    int Position,
+    char ToAminoAcid,
+
+    // AlphaMissense
+    double AmPathogenicity,
+    string AmClass,
+    bool AmLabel,
+
+    // ESM1b
+    double Esm1bLlr,
+    string Esm1bIsPathogenic,
+
+    // Stability
+    double? PredDdg,
+    bool PredDdgLabel,
+
+    // Interface
+    double? InterfacePdockq,
+    bool InterfaceLabel,
+
+    // Pocket
+    bool PocketLabel,
+
+    // Mechanistic
+    string MechanisticLabel
+);
+}
