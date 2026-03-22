3faa407971b087a63a1d1cbbf5bdd6249626fb4c
todor-grigorov <t.grigorov99@gmail.com>
2026-03-22 17:55:43 +0100

    feat: implement ServiceManager (Application)

---
+++ backend/ProteinMutation/src/ProteinMutation.Application/Services/ServiceManager.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using ProteinMutation.Application.Abstractions.Services;
+using ProteinMutation.Domain.Repositories;
+
+namespace ProteinMutation.Application.Services
+{
+    public sealed class ServiceManager : IServiceManager
+    {
+        private readonly Lazy<IVariantsService> _variantsService;
+        private readonly Lazy<IProteinsService> _proteinsService;
+
+        public ServiceManager(IProteinVariantRepository repository, IMapper mapper)
+        {
+            _variantsService = new Lazy<IVariantsService>(() => new VariantsService(repository, mapper));
+            _proteinsService = new Lazy<IProteinsService>(() => new ProteinsService(repository, mapper));
+        }
+
+        public IVariantsService VariantsService => _variantsService.Value;
+
+        public IProteinsService ProteinsService => _proteinsService.Value;
+    }
+}
