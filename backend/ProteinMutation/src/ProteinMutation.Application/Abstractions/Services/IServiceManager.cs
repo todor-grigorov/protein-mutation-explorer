55f3e2f2206db4a89d85da6a3762d1ac5d16f164
todor-grigorov <t.grigorov99@gmail.com>
2026-03-22 17:53:33 +0100

    feat: add IServiceManager (Application)

---
+++ backend/ProteinMutation/src/ProteinMutation.Application/Abstractions/Services/IServiceManager.cs
@@ -0,0 +1,8 @@
+﻿namespace ProteinMutation.Application.Abstractions.Services
+{
+    public interface IServiceManager
+    {
+        IVariantsService VariantsService { get; }
+        IProteinsService ProteinsService { get; }
+    }
+}
