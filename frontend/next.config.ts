508e0b3cbf44bde8de16f87b46abfc679e685f8e
todor-grigorov <t.grigorov99@gmail.com>
2026-03-23 18:24:14 +0100

    feat: init setup of the frontend project

---
+++ frontend/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.externals = [...(config.externals || []), { canvas: 'canvas' }]
+    return config
+  },
+}
+
+export default nextConfig
